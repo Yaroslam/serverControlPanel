e4f3bbbbc4432980a7cd70557816ea95626734f0
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-10-21 19:02:46 +0300

    fix

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -7,6 +7,8 @@ use Happy\ServerControlPanel\Session\Connection\Connector;
 class Session
 {
     private Connector $connector;
+    private bool $ifResult;
+    private array $chainContext;
 
     public function __construct(ConnectionInterface $connectionType, array $connectProperties)
     {
@@ -20,10 +22,45 @@ class Session
         $errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
         stream_set_blocking($errorStream, true);
         stream_set_blocking($stream, true);
-        echo "Output: " . stream_get_contents($stream);
-        echo "Error: " . stream_get_contents($errorStream);
         fclose($errorStream);
         fclose($stream);
+        return ["output" => stream_get_contents($stream), "error" => stream_get_contents($errorStream)];
+    }
+
+
+    public function if(string $cmdCommand, string $ifCondition, string $mustIn="output")
+    {
+        $execRes = $this->exec($cmdCommand);
+        $this->ifResult = preg_match("/$ifCondition/", $execRes[$mustIn]);
+        return $this;
+    }
+
+    public function then(string $cmdCommand)
+    {
+        if($this->ifResult)
+        {
+            $this->chainContext = $this->exec($cmdCommand);
+        }
+        return $this;
+    }
+
+    public function else(string $cmdCommand)
+    {
+        if(!$this->ifResult){
+            $this->chainContext = $this->exec($cmdCommand);
+        }
+        return $this;
+    }
+
+    public function apply()
+    {
+        $this->NullContext();
+        return $this->chainContext;
+    }
+
+    private function NullContext()
+    {
+        unset($this->ifResult);
     }
 
     public function __destruct()
