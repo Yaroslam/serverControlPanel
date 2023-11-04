a1f9c9d0811e3c0a89c8967b354cbd0cad18c612
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-11-04 15:16:20 +0300

    Refactored ChainSession class to implement command chaining and error handling

--- src/Session/Commands/NoneCommand.php
+++ src/Session/Commands/NoneCommand.php
@@ -5,4 +5,9 @@ namespace Happy\ServerControlPanel\Session\Commands;
 class NoneCommand extends BaseCommand
 {
     protected CommandClasses $commandType = CommandClasses::None;
+
+    public function execution($shell)
+    {
+        return null;
+    }
 }
