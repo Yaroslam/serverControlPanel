b36d3a6a9fd37ccc5bb89158a08a1f5e402a52bf
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-11-10 21:30:07 +0300

    Refactored ChainSession class to implement command chaining and error handling

--- src/Session/ChainSession.php
+++ src/Session/ChainSession.php
@@ -100,7 +100,7 @@ class ChainSession extends AbstractSession
         $this->lastOperator->addToBody($this->operatorsGraph[$this->deepLevel], 'then');
         $this->lastCommand = $this->lastOperator;
         var_dump($this->deepLevel);
-
+        var_dump($this->lastCommand);
         return $this;
 
     }
