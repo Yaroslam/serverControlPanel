42a11b52debc4d99f8f63a818e2acf90149b4818
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-12-03 19:25:22 +0300

    error fixed

--- src/Session/Commands/ExecCommand.php
+++ src/Session/Commands/ExecCommand.php
@@ -13,6 +13,7 @@ class ExecCommand extends BaseCommand
 
     // TODO
     //  переделать на стримы эрроров и вывода или stream_get_mate_data
+    //  https://stackoverflow.com/questions/10478491/php-ssh2-exec-channel-exit-status
     public function execution($shell)
     {
         var_dump('exec '.$this->commandText);
