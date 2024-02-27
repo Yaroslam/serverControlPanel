d050ec9815682e40556f67b7347d9ddabd3069f2
Черкашин Ярослав Ю. <iaiucherkashin@f-soft.org>
2024-02-28 00:13:52 +0300

    docs()

---
+++ test/ConnectionsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace test;
+
+require_once __DIR__.'/../vendor/autoload.php';
+use PHPUnit\Framework\TestCase;
+use Yaroslam\SSH2\Session\Connection\UserPasswordConnection;
+
+// TODO
+//  переменные в блоке документации
+class ConnectionsTest extends TestCase
+{
+    public function testUserPasswordConn()
+    {
+        $connector = new UserPasswordConnection();
+        $this->assertTrue($connector->connect(ssh2_connect('194.87.110.114', 22),
+            ['user' => '', 'password' => '']), 'error during connect by password and login');
+    }
+}
